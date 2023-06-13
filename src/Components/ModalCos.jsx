58d401676b9e88e25f27bf1ffe0a3aad8472d3e9
Paul Geambazu <paul.geambazu@kingsbridge.co.uk>
2023-06-14 00:03:33 +0100

    total price of basket

--- src/Components/ModalCos.jsx
+++ src/Components/ModalCos.jsx
@@ -1,49 +1,93 @@
-import React from 'react'
+import React from 'react';
 
-function ModalCos({closModal, cosulCumparaturi, deleteProdus}) {
+function ModalCos({ closeModal, cosulCumparaturi, deleteProdus }) {
+  // Extract the prices from the objects in cosulCumparaturi
+   const total = cosulCumparaturi.reduce((acc, unElement) => {
+    const price = parseInt(unElement.price.replace(/\D/g, ''));
+    return acc + price;
+  }, 0);
+
   return (
-    <div style={{ zIndex: '1000',
-    position: 'fixed',
-    top: '0',
-    left: '0',
-    right: '0',
-    bottom: '0',
-    width: '100%',
-    height: '100%',
-    background: 'rgba(0, 0, 0, 0.7)'}}>
-        <div style={{
-  position: 'fixed',
-  width:'60%',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#fff',
-  borderRadius: '6px',
-  color: '#000',
-  display: 'flex',
-  flexDirection: 'column',
-        }}>
-            {cosulCumparaturi?.length ?
+    <div>
+      <div
+        style={{
+          zIndex: '1000',
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          right: '0',
+          bottom: '0',
+          width: '100%',
+          height: '100%',
+          background: 'rgba(0, 0, 0, 0.7)',
+        }}
+      >
+        <div
+          style={{
+            position: 'fixed',
+            width: '60%',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#fff',
+            borderRadius: '6px',
+            color: '#000',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          {cosulCumparaturi?.length ? (
             <div>
-{
-        cosulCumparaturi?.map((unElement, index) => {
-            return (
-            <div style={{ borderRadius: '5px', border: '1px solid', background: '#c4c4c4' }}key={index}><div><img style={{width:'350px', height:'200px'}} src={`${unElement?.picture}`}  alt={unElement?.picture}  />{unElement?.name}</div><div>{unElement?.descriere}</div>
-            <button onClick={(e) => deleteProdus(unElement?.id)}>remove this</button>
+              {cosulCumparaturi?.map((unElement, index) => (
+                <div
+                  style={{
+                    borderRadius: '5px',
+                    border: '1px solid',
+                    background: '#c4c4c4',
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    padding: '10px',
+                  }}
+                  key={index}
+                >
+                  <img
+                    style={{ width: '100px', height: '100px', marginRight: '10px' }}
+                    src={`${unElement?.picture}`}
+                    alt={unElement?.picture}
+                  />
+                  <div>
+                    <div>{unElement?.name}</div>
+                    <div>{unElement?.descriere}</div>
+                  </div>
+                  <button onClick={(e) => deleteProdus(unElement?.id)}>
+                    remove this
+                  </button>
+                </div>
+              ))}
             </div>
-            )
-        })
-        }
-        </div> :
-        <div>Inca nu ai selectat nimic</div>}
-</div>
-<button  style={{
-  position: 'fixed',
-  width:'20px',
-  left: '75%',
-  top: '49%'}} onClick={closModal}>x</button>
+          ) : (
+            <div>Inca nu ai selectat nimic</div>
+          )}
+          {cosulCumparaturi?.length ? (
+            <div style={{ marginTop: '10px' }}>
+              Prices: {total} {/* Display the prices */}
+            </div>
+          ) : null}
+          <button
+            style={{
+              position: 'absolute',
+              top: '5px',
+              right: '5px',
+            }}
+            onClick={closeModal}
+          >
+            x
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ModalCos
+export default ModalCos;
